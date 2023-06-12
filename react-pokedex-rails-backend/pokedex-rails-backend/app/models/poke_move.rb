49fede1549d03418137d52b10cda55049f323cb3
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-06-12 15:18:20 -0700

    got down validations for the pokemon

--- react-pokedex-rails-backend/pokedex-rails-backend/app/models/poke_move.rb
+++ react-pokedex-rails-backend/pokedex-rails-backend/app/models/poke_move.rb
@@ -1,4 +1,15 @@
+# == Schema Information
+#
+# Table name: poke_moves
+#
+#  id         :bigint           not null, primary key
+#  pokemon_id :bigint           not null
+#  move_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class PokeMove < ApplicationRecord
-  belongs_to :pokemon
-  belongs_to :move
+  
+  belongs_to :pokemon, dependent: :destroy
+  belongs_to :move, dependent: :destroy
 end
--- react-pokedex-rails-backend/pokedex-rails-backend/app/models/pokemon.rb
+++ react-pokedex-rails-backend/pokedex-rails-backend/app/models/pokemon.rb
@@ -1,2 +1,48 @@
+# == Schema Information
+#
+# Table name: pokemons
+#
+#  id         :bigint           not null, primary key
+#  number     :integer          not null
+#  name       :string           not null
+#  attack     :integer          not null
+#  defense    :integer          not null
+#  poke_type  :string           not null
+#  image_url  :string           not null
+#  captured   :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Pokemon < ApplicationRecord
+
+    TYPES = [
+    'fire',
+    'electric',
+    'normal',
+    'ghost',
+    'psychic',
+    'water',
+    'bug',
+    'dragon',
+    'grass',
+    'fighting',
+    'ice',
+    'flying',
+    'poison',
+    'ground',
+    'rock',
+    'steel'
+  ].sort.freeze
+
+    validates :number, :name, :attack, :defense, :poke_type, :image_url, :captured, presence: true
+    validates :captured, inclusion: { in: [true, false] }
+    validates :name, length: { in: 3..255 }, uniqueness: { message: "'%{value}' is already in use" }
+    validates :number, numericality: { greater_than: 0 }, uniqueness: { message: "'%{value}' is already in use" }
+    validates :attack, numericality: { greater_than: 0, less_than: 100 }
+    validates :defence, numericality: { greater_than: 0, less_than: 100 }
+    validates :poke_type, inclusion: { in: TYPES, message: "'%{value}' is not a valid pokemon type" }
+
+
+
+
 end
